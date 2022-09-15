fdeb6a7d8bf3d26d96237f23a86c07a7adac95d8
tolumorohunfola <111201704+tolumorohunfola@users.noreply.github.com>
2022-09-15 01:19:23 +0100

    making flask work and beginning to set up suitable files structure

--- pokemon.py
+++ pokemon.py
@@ -1,1 +1,15 @@
-print('Hello, World!')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('mainPage.html')
+# put a button in here to lead to different pages
+
+@app.route("/view-my-pokemon")
+def view_pokemon():
+    return "This is your pokemons :)"
+
+if __name__ == "__main__":
+    app.run()

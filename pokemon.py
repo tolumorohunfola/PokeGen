32a1dd21a511fb0628af86127ef5039d339b3427
tolumorohunfola <111201704+tolumorohunfola@users.noreply.github.com>
2022-10-02 00:29:21 +0100

    created two more pages for the website and wrote the code for the function to pull the information about random pokemon - next step is to get the variables from the python file called information about pokemons into the random_pokemon.html file.

--- pokemon.py
+++ pokemon.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template('mainPage.html')
+@app.route("/")  # the slash means that when we go to the default page, the below is displayed
+def home():  # this line defines each page's function - very useful
+    return render_template('mainPage.html') #this means tells the app to use the code in the html file
 # put a button in here to lead to different pages
 
-@app.route("/view-my-pokemon")
-def view_pokemon():
-    return "This is your pokemons :)"
+@app.route('/random')
+def  randomPokemon():
+    return render_template('random_poke.html')
+
+
+@app.route("/my-pokemon")
+def my_pokemon():
+    return render_template('my_pokemon.html')
 
 if __name__ == "__main__":
     app.run()
